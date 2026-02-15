22c6ae3dbb3b39652034582b0075e9374eab6163
Rama Devi <rdhelenkly@gmail.com>
2026-02-15 19:20:07 +0000

    initial infrastructure

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "server"
+  }
+}
